6a5d8bac82e0687c2b54b4f70ceb94ec63ef3531
muddassirali8089 <114619571+muddassirali8089@users.noreply.github.com>
2025-07-07 06:55:27 +0500

    the tailwind is done
    the tailwind is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,52 @@
-import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import AppLayout from './ui/AppLayout'
-import Home from './ui/Home'
-import Menu ,  {loader as menuLoader} from './Features/menu/Menu'
-import Cart from './Features/cart/Cart'
-import CreateOrder , {action as createOrderAction } from './Features/order/CreateOrder'
-import Order  , {loader as orderLoader} from "./Features/order/Order.jsx";
-// import SearchOrder  from './Features/order/SearchOrder.jsx'
-import Error from "./ui/Error.jsx"
+import Home from './ui/Home';
+import Error from './ui/Error';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
+import Cart from './features/cart/Cart';
+import CreateOrder, {
+  action as createOrderAction,
+} from './features/order/CreateOrder';
+import Order, { loader as orderLoader } from './features/order/Order';
+import { action as updateOrderAction } from './features/order/UpdateOrder';
+
+import AppLayout from './ui/AppLayout';
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
-    errorElement: <Error/>,
+    errorElement: <Error />,
+
     children: [
       {
-        path :"/",
+        path: '/',
         element: <Home />,
       },
       {
-        path: "menu",
+        path: '/menu',
         element: <Menu />,
-        loader : menuLoader,
-        errorElement: <Error/>,
+        loader: menuLoader,
+        errorElement: <Error />,
       },
+      { path: '/cart', element: <Cart /> },
       {
-        path: "cart",
-        element: <Cart />,
-      },
-      {
-        path: "order/new",
+        path: '/order/new',
         element: <CreateOrder />,
         action: createOrderAction,
       },
       {
-        path: "order/:orderId", 
+        path: '/order/:orderId',
         element: <Order />,
-        loader:orderLoader,
-        errorElement: <Error/>,
-
-       
+        loader: orderLoader,
+        errorElement: <Error />,
+        action: updateOrderAction,
       },
     ],
   },
-])
+]);
 
 function App() {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import Header from './Header'
-import CartOverview from '../Features/cart/CartOverview'
-import { Outlet, useNavigation } from 'react-router-dom'
-
-import Loader from './Loader'
+import Header from './Header';
+import Loader from './Loader';
+import CartOverview from '../features/cart/CartOverview';
+import { Outlet, useNavigation } from 'react-router-dom';
 
 function AppLayout() {
-const navigation = useNavigation()
-
-  const isLoading = navigation.state === "loading";
+  const navigation = useNavigation();
+  const isLoading = navigation.state === 'loading';
 
   return (
-    <div className='layout'>
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      {isLoading && <Loader />}
 
-      {isLoading && <Loader/>}
+      <Header />
 
-        <Header/>
+      <div className="overflow-y-auto">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
 
-        <main> <Outlet/></main>
-
-        <CartOverview/>
+      <CartOverview />
     </div>
-  )
+  );
 }
 
-export default AppLayout
+export default AppLayout;
